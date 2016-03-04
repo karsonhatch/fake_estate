0668edd062eec85ad7e143aff382f389ad7d8e30
Karson Hatch <karsonhatch@gmail.com>
2016-03-04 13:52:59 -0700

    initial commit

---
+++ fake_estate/config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+
+roots 'sellers#index'
+
+  resouces :sellers do 
+    resouces :homes do 
+      resources :addresses
+    end
+  end
+end
---
+++ fake_estate/db/migrate/20160304203659_create_sellers.rb
@@ -0,0 +1,12 @@
+class CreateSellers < ActiveRecord::Migration
+  def change
+    create_table :sellers do |t|
+      t.string :name, null: false
+      t.string :phone, null: false
+      t.string :email, null: false
+      
+
+      t.timestamps null: false
+    end
+  end
+end
